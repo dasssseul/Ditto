007ea24c165da7c23313a192db6fe5323552a6c5
dasssseul <daseul8086@gmail.com>
2023-07-22 11:16:17 +0900

    feat : 홈 화면 수정 및 메뉴 툴팁 추가

--- src/component/Menu/index.jsx
+++ src/component/Menu/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.css';
 import { HiDocumentSearch, HiChartBar, HiChat, HiHome } from 'react-icons/hi';
+import { Tooltip } from 'react-tooltip';
 
 const Menu = () => {
   return (
     <div className="menu-container">
-      <div className="logo">Ditto</div>
+      <NavLink to="/" className="link">
+        <div className="logo">Ditto</div>
+      </NavLink>
       <div className="gnb-menus">
         <NavLink
           to="/"
@@ -18,32 +21,42 @@ const Menu = () => {
           </button>
         </NavLink>
         <NavLink
-          to="/about"
+          to="/talk"
           className={(({ isActive }) => isActive && 'active', 'link')}
+          data-tooltip-id="talk"
+          data-tooltip-content="Ditto와 대화를 나눕니다."
         >
           <button className="gnb-menu">
-            <HiDocumentSearch color="white" size={20} />
-            <span>About</span>
+            <HiChat color="white" size={20} />
+
+            <span>Talk</span>
           </button>
         </NavLink>
+        <Tooltip id="talk" />
         <NavLink
           to="/analysis"
           className={(({ isActive }) => isActive && 'active', 'link')}
+          data-tooltip-id="analysis"
+          data-tooltip-content="Ditto를 통해 수집된 대화를 분석합니다."
         >
           <button className="gnb-menu">
             <HiChartBar color="white" size={20} />
             <span>Analysis</span>
           </button>
         </NavLink>
+        <Tooltip id="analysis" />
         <NavLink
-          to="/talk"
+          to="/about"
           className={(({ isActive }) => isActive && 'active', 'link')}
+          data-tooltip-id="about"
+          data-tooltip-content="위안부 역사와 Ditto에 대해 설명합니다."
         >
           <button className="gnb-menu">
-            <HiChat color="white" size={20} />
-            <span>Talk</span>
+            <HiDocumentSearch color="white" size={20} />
+            <span>About</span>
           </button>
         </NavLink>
+        <Tooltip id="about" />
       </div>
     </div>
   );
